1fec29e1449e77b8411bb6cbdea7875ccb8e790d
Javier Gil <javier.gil.rodriguez.contractor@bbva.com>
2026-03-02 16:28:35 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,13 +71,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KM7KRF66');` }} />
+        {/* End Google Tag Manager */}
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <meta name="theme-color" content="#FF5733" />
         <link rel="icon" href="/browser_tab.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/ios.svg" />
         <link rel="manifest" href="/manifest.json" />
       </head>
-      <body className={`${inter.variable} ${newsreader.variable} font-inter`}>{children}</body>
+      <body className={`${inter.variable} ${newsreader.variable} font-inter`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KM7KRF66" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   )
 }
